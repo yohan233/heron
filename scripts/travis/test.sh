5f29805356e8d7ba63fa1ece1b09215bb363a8e5
Huijun Wu <huijunw@twitter.com>
2017-04-28 00:04:46 -0700

    add tmaster unit test togethe

--- scripts/travis/test.sh
+++ scripts/travis/test.sh
@@ -6,7 +6,7 @@ DIR=`dirname $0`
 # Run server:stmgr_unittest
 for i in `seq 1 100`; do
   echo === repeat test $i ===
-  bazel --bazelrc=tools/travis-ci/bazel.rc test --config=ubuntu --nocache_test_results //heron/stmgr/tests/cpp/server/...
+  bazel --bazelrc=tools/travis-ci/bazel.rc test --config=ubuntu --nocache_test_results --test_tag_filters=flaky heron/...
   RESULT=$?
   if [ $RESULT -ne 0 ]; then
     # Dump out stream manager log
