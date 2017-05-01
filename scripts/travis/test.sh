f16096ebf1b97f943424227487d8ee6c6b930296
Huijun Wu <huijunw@twitter.com>
2017-05-01 16:27:23 -0700

    restrict test target

--- scripts/travis/test.sh
+++ scripts/travis/test.sh
@@ -6,7 +6,7 @@ DIR=`dirname $0`
 # Run server:stmgr_unittest
 for i in `seq 1 100`; do
   echo === repeat test $i ===
-  bazel --bazelrc=tools/travis-ci/bazel.rc test --config=ubuntu --nocache_test_results --test_tag_filters=flaky heron/...
+  bazel --bazelrc=tools/travis-ci/bazel.rc test --config=ubuntu --nocache_test_results --test_output=all //heron/stmgr/tests/cpp/server/...
   RESULT=$?
   if [ $RESULT -ne 0 ]; then
     # Dump out stream manager log
